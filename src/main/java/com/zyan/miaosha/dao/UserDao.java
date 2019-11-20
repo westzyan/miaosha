618ab5439b48291a11e3b3ca84c4d1eb4af9f6bd
westzyan <15639067131@163.com>
2019-11-20 21:22:26 +0800

    秒杀系统初始化

---
+++ src/main/java/com/zyan/miaosha/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.zyan.miaosha.dao;
+
+/**
+ * @author zyan
+ * @version 1.0
+ * @date 19-11-19 下午10:33
+ */
+public class UserDao {
+}
---
+++ src/main/java/com/zyan/miaosha/service/UserService.java
@@ -0,0 +1,9 @@
+package com.zyan.miaosha.service;
+
+/**
+ * @author zyan
+ * @version 1.0
+ * @date 19-11-19 下午10:35
+ */
+public class UserService {
+}
