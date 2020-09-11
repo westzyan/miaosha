8752cfafce7960c54a52eb7434e5a6f7c4f3c011
westzyan <15639067131@163.com>
2020-09-11 16:43:46 +0800

    加入了SQL

--- src/main/java/com/zyan/miaosha/result/CodeMsg.java
+++ src/main/java/com/zyan/miaosha/result/CodeMsg.java
@@ -18,8 +18,8 @@ public class CodeMsg {
 	public static CodeMsg MOBILE_ERROR = new CodeMsg(500213, "手机号格式错误");
 	public static CodeMsg MOBILE_NOT_EXIST = new CodeMsg(500214, "手机号不存在");
 	public static CodeMsg PASSWORD_ERROR = new CodeMsg(500215, "密码错误");
-	
-	
+	public static CodeMsg REPEAT_MIAOSHA = new CodeMsg(500216, "重复秒杀");
+
 	//商品模块 5003XX
 	
 	
